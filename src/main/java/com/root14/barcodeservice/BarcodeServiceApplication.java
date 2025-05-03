952d865ead3f183cedb0adfeea621933d30d2311
ilkay gavaz <ilkaygavaz2@gmail.com>
2025-05-03 12:23:00 +0300

    initial commit

---
+++ src/main/java/com/root14/barcodeservice/BarcodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.root14.barcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/root14/barcodeservice/BarcodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root14.barcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
