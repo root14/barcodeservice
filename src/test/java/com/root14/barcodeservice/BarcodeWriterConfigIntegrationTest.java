ff97955718b919e97b92b6e31589ef28443bf911
root14 <ilkaygavaz2@gmail.com>
2025-06-09 13:03:09 -0700

    refactor openapi endpoint

--- src/test/java/com/root14/barcodeservice/BarcodeWriterConfigIntegrationTest.java
+++ src/test/java/com/root14/barcodeservice/BarcodeWriterConfigIntegrationTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import static org.assertj.core.api.Assertions.assertThat; // Or use JUnit's Assertions
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class BarcodeWriterConfigIntegrationTest {
